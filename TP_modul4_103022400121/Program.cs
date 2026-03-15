c5cab0572f7d6efc54e565c191c5bc1f1507a81a
RayyanMFPP <rayyantelyu@gmail.com>
2026-03-16 04:33:19 +0700

    menambahkan class Doormachine

--- TP_modul4_103022400121/Program.cs
+++ TP_modul4_103022400121/Program.cs
@@ -1,27 +1,39 @@
-﻿public class KodePos
+﻿namespace TP_modul4_103022400121
 {
-    // Ini adalah "Tabel" nya
-    private Dictionary<string, string> dic = new Dictionary<string, string>()
+    public class KodePos
     {
-        {"Batununggal", "40266"},
-        {"Kujangsari",  "40287"},
-        {"Mengger",     "40267"},
-        {"Wates",       "40256"},
-        {"Cijaura",     "40287"},
-        {"Jatisari",    "40286"},
-        {"Margasari",   "40286"},
-        {"Sekejati",    "40286"},
-        {"Kebonwaru",   "40272"},
-        {"Maleer",      "40274"}
-    };
-
-    public string getKodePos(string kelurahan)
-    {
-        if (dic.ContainsKey(kelurahan))
+        private Dictionary<string, string> dic = new Dictionary<string, string>()
         {
-            return dic[kelurahan];
+            {"Batununggal", "40266"}, {"Kujangsari", "40287"}, {"Mengger", "40267"},
+            {"Wates", "40256"}, {"Cijaura", "40287"}, {"Jatisari", "40286"},
+            {"Margasari", "40286"}, {"Sekejati", "40286"}, {"Kebonwaru", "40272"},
+            {"Maleer", "40274"}
+        };
+
+        public string getKodePos(string kelurahan)
+        {
+            return dic.ContainsKey(kelurahan) ? dic[kelurahan] : "Tidak ditemukan";
         }
-        return "Kelurahan tidak ditemukan";
+    }
+
+    public class DoorMachine
+    {
+        public enum State { Terkunci, Terbuka };
+        public State currentState = State.Terkunci;
+
+        public void PerubahanState(string command)
+        {
+            if (currentState == State.Terkunci && command == "BukaPintu")
+            {
+                currentState = State.Terbuka;
+                Console.WriteLine("Pintu tidak terkunci");
+            }
+            else if (currentState == State.Terbuka && command == "KunciPintu")
+            {
+                currentState = State.Terkunci;
+                Console.WriteLine("Pintu terkunci");
+            }
+        }
     }
 }
 
